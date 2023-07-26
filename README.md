dfcddf373947e76625bd918296f3aa5d70466d11
ChenYing Kuo <evshary@gmail.com>
2023-07-26 17:04:37 +0800

    Support using ThroughputSubscriber.

--- README.md
+++ README.md
@@ -2,10 +2,32 @@
 
 # Usage
 
+## Subscribe
+
 * Build CycloneDDS Throughput example and run
 
 ```shell
 ./build/bin/ThroughputPublisher 8 0 1 0 ""
+```
+
+* Build zenoh-bridge-dds and run (Use 0.7.0-rc)
+
+```shell
+./target/release/zenoh-bridge-dds
+```
+
+* Run
+
+```shell
+cargo run
+```
+
+## Publish
+
+* Build CycloneDDS Throughput example and run
+
+```shell
+./build/bin/ThroughputSubscriber 0 0 ""
 ```
 
 * Build zenoh-bridge-dds and run (Use 0.7.0-rc)
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use cdr::{CdrLe, Infinite};
 use serde_derive::{Deserialize, Serialize};
 use zenoh::prelude::r#async::*;
 
-#[derive(Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct DataType {
     count: u64,
     payload: Vec<u8>,
@@ -14,6 +14,17 @@ async fn main() {
     println!("Opening session...");
     let session = zenoh::open(Config::default()).res().await.unwrap();
     let key = String::from("Throughput");
+
+    // publisher
+    let publisher = session.declare_publisher(key.clone()).res().await.unwrap();
+    let data = DataType {
+        count: 0,
+        payload: vec![97, 97, 97, 97, 97, 97, 97, 97],
+    };
+    let encoded = cdr::serialize::<_, _, CdrLe>(&data, Infinite).unwrap();
+    if let Err(e) = publisher.put(encoded).res().await {
+        println!("Error writing {}: {}", publisher.key_expr().as_str(), e);
+    }
 
     // subscriber
     let subscriber = session.declare_subscriber(key).res().await.unwrap();
